ef30f76cd7a930a7db25888e0fc1d39348dbd62d
gg10bronco-cpu <gg10bronco@gmail.com>
2025-11-10 09:19:58 -0600

    Add files via upload

---
+++ lab07/script.js
@@ -0,0 +1,14 @@
+const alertMe = document.getElementById("entrybutton");
+const input = document.getElementById("entryinput");
+const output = document.getElementById("textoutput");
+var value;
+
+function init(){
+  alertMe.addEventListener("click", function() {
+    value = input.value;
+    output.textContent = value;
+    alert("Gilberto: " + value);
+  });
+}
+
+window.addEventListener('load', init);
